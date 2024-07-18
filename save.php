7d5aeadfbdd4e02ce92bb066e1c7c5690c21b6de
quyet11 <100986539+quyet11@users.noreply.github.com>
2024-07-19 00:33:43 +0700

    Delete save.php

--- save.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $data = $_POST['data'];
-    file_put_contents('data.json', $data);
-    echo json_encode(['status' => 'success']);
-} else {
-    echo json_encode(['status' => 'error', 'message' => 'Invalid request']);
-}
-?>
